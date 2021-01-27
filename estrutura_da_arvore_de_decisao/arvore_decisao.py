c1ab55e5e51eb49e8fa7219b53979f03c27607be
GigasMesh <40374830+GigasMesh@users.noreply.github.com>
2021-01-27 15:35:41 -0300

    Feat: Cálculo de ganho de informação

--- estrutura_da_arvore_de_decisao/arvore_decisao.py
+++ estrutura_da_arvore_de_decisao/arvore_decisao.py
@@ -1,6 +1,7 @@
 import math
 from collections import Counter
 
+
 class Arvore_Decisao(object):
     
     def __init__(self,banco_de_dados,numero_amostras):
@@ -9,25 +10,59 @@ class Arvore_Decisao(object):
         self.bd = banco_de_dados.drop(columns=['name','religion']).head(self.numero_amostras)
         self.alvo = banco_de_dados['religion'].head(self.numero_amostras)
         
-        self.entropia_bd = 0
-        self.calcula_entropia()
+        self.entropia_bd = self.calcula_entropia([contagem['freq'] for contagem in self.contagem(self.alvo).values()])
+        self.ganhos = {}
+        
+        for atributo in ['stripes','bars','circles','crosses']:
+            self.calcula_info_necessaria(atributo)
+        print(self.ganhos)
         
     
-    def calcula_entropia(self):
-        contagem_religioes = self.contagem(self.alvo)
-        self.entropia_bd = 0
-        for contagem in contagem_religioes.values():
-            cardionalidade = (contagem / self.numero_de_amostras)
-            self.entropia += -cardionalidade*math.log(cardionalidade,2)
-            print('-({}/{})log({}/{}) = {}'.format(contagem,self.numero_amostras,contagem,self.numero_amostras,
-                                                -cardionalidade*math.log(cardionalidade,2)))
-
+    
     def contagem(self, series):
         contagem_dict = {}
         for index, item in enumerate(series):
             if item not in contagem_dict:
-                contagem_dict[item] = {freq: 1, indexes: [index]}
+                contagem_dict[item] = {'freq': 1, 'indexes': [index]}
             else: 
-                contagem_dict[item][freq] += 1
-                contagem_dict[item][indexes].append(index)
+                contagem_dict[item]['freq'] += 1
+                contagem_dict[item]['indexes'].append(index)
         return contagem_dict
+    
+    
+    def contagem_atributo(self, series):
+        contagem_dict = {}
+        for index, item in enumerate(series):
+            if item not in contagem_dict:
+                contagem_dict[item] = {'freq': 1, self.alvo[index]: 1}
+            elif self.alvo[index] not in contagem_dict[item]:
+                contagem_dict[item][self.alvo[index]] = 1
+                contagem_dict[item]['freq'] += 1
+            else:
+                contagem_dict[item][self.alvo[index]] += 1
+                contagem_dict[item]['freq'] += 1
+        return contagem_dict
+            
+    
+    def calcula_entropia(self, valores):
+
+        entropia = 0
+        for contagem in valores:
+            cardinalidade = (contagem / self.numero_amostras)
+            entropia += - cardinalidade * math.log(cardinalidade, 2)
+            #print('-({}/{})log({}/{}) = {}'.format(contagem, self.numero_amostras,contagem, self.numero_amostras,
+            #                                       - cardinalidade * math.log(cardinalidade, 2)))
+        return entropia
+        
+    
+    def calcula_info_necessaria(self, atributo):
+        
+        contagem_atributo = self.contagem_atributo(self.bd[f'{atributo}'])
+        ganho_atributo = 0
+        
+        for contagem in contagem_atributo.values():
+            cardinalidade = (contagem['freq'] / self.numero_amostras)
+            ganho_atributo += cardinalidade * self.calcula_entropia(list(contagem.values())[1:])
+            
+        ganho_atributo = self.entropia_bd - ganho_atributo
+        self.ganhos[atributo] = ganho_atributo
---
+++ executar_arvore_decisao.py
@@ -0,0 +1,4 @@
+from estrutura_da_arvore_de_decisao import Arvore_Decisao
+from bancos_de_dados import bd_bandeiras
+
+ad = Arvore_Decisao(bd_bandeiras,100)
