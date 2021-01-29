f67d39ed103e8a551318d90d7fc4f5bd6c76551d
GigasMesh <40374830+GigasMesh@users.noreply.github.com>
2021-01-28 21:09:41 -0300

    Refactor: BD_Vinho

--- bancos_de_dados/bd_vinho.py
+++ bancos_de_dados/bd_vinho.py
@@ -4,5 +4,5 @@ import numpy as np
 colunas = open('bancos_de_dados/colunas_vinho.txt', 'r')
 nome_colunas = [coluna[:-1] for coluna in colunas]
 colunas.close()
-bd_vinho = pd.read_csv('bancos_de_dados/banco_vinho.csv', names=nome_colunas, header=None)
-bd_vinho.reset_index(inplace=True)
+bd_vinho = pd.read_csv('bancos_de_dados/banco_vinho.csv',names=nome_colunas, header=None)
+print(bd_vinho)
