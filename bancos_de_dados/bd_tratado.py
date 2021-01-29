16f14988d9b658437863b192d91078fcc9b57b46
Iuryglabson <iuryglabson18@gmail.com>
2021-01-29 13:57:07 -0300

    Refactor: Generalização do Banco

--- bancos_de_dados/bd_tratado.py
+++ bancos_de_dados/bd_tratado.py
@@ -1,24 +1,27 @@
-from bancos_de_dados.bd_vinho import bd_vinho
 from bancos_de_dados.ferramentas import *
 import numpy as np
 import pandas as pd
 
-def tratar_bd(banco):
-    tipos_saidas = bd_vinho['Wine'].unique()
+
+def tratar_bd(banco, coluna):
+    tipos_saidas = banco[coluna].unique()
     base_treino = np.array([])
     base_teste = np.array([])
+    tamanho_treino = 0
+    tamanho_teste = 0
     for classe in (tipos_saidas):
-        banco_auxiliar = banco.query('Wine==%d' %classe)
+        banco_auxiliar = banco.query('%s==%d' %(coluna, classe))
         treino_auxiliar = retorna_treino(banco_auxiliar)
-        #print(len(treino_auxiliar))
         for linha in (treino_auxiliar.values):
             base_treino = np.concatenate((base_treino, linha), axis=0)
         teste_auxiliar = retorna_teste(banco_auxiliar, treino_auxiliar)
         for linha in (teste_auxiliar.values):
             base_teste = np.concatenate((base_teste, linha), axis=0)
-
-    base_treino = np.reshape(base_treino, (125, 14))
-    base_teste = np.reshape(base_teste, (53, 14))
+        tamanho_treino += len(treino_auxiliar)
+        tamanho_teste += len(teste_auxiliar)
+    num_colunas = len(banco.columns)
+    base_treino = np.reshape(base_treino, (tamanho_treino, num_colunas))
+    base_teste = np.reshape(base_teste, (tamanho_teste, num_colunas))
     base_treino = pd.DataFrame(base_treino)
     base_teste = pd.DataFrame(base_teste)
 
--- executar_rede_neural.py
+++ executar_rede_neural.py
@@ -1,11 +1,15 @@
 from arquiteturas_rede_neural import criar_arquitetura_4
+from arquiteturas_rede_neural import criar_arquitetura_2
 from bancos_de_dados.bd_tratado import *
 
-arquitetura = criar_arquitetura_4()
+arquitetura = criar_arquitetura_2()
 num_epocas = 500
+learning_rate = 0.5
+coluna_alvo = 'vinho'  # continente, vinho, vidro, iris
 
 rede = arquitetura
-rede.learning_rate = 0.5
+rede.learning_rate = learning_rate
 rede.insere_sinapses_e_bias()
-base_treino, base_teste, tipos_saidas = tratar_bd(rede.banco)
+banco = rede.banco
+base_treino, base_teste, tipos_saidas = tratar_bd(banco, coluna_alvo)
 rede.aprender(num_epocas, base_treino)
